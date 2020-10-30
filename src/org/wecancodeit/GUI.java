6a8a810a85c397b118d05a5a033226648d94c85f
fpemba <fatuma.pemba@yahoo.com>
2020-10-29 21:37:37 -0400

    added GUI class

---
+++ src/org/wecancodeit/GUI.java
@@ -0,0 +1,39 @@
+package org.wecancodeit;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements  ActionListener{
+
+        private int count = 0;
+        private JLabel label;
+        private JPanel panel;
+
+        public GUI() {
+            JFrame frame = new JFrame();
+            JButton button = new JButton("Click me");
+            button.addActionListener(this);
+
+            label = new JLabel("Number of clicks: 0");
+
+            panel = new JPanel();
+            panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+            panel.setLayout(new GridLayout(0, 1));
+            panel.add(button);
+            panel.add(label);
+
+            frame.add(panel, BorderLayout.CENTER);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setTitle("My GUI");
+            frame.pack();
+            frame.setVisible(true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            count++;
+            label.setText("Number of clicks: " + count);
+        }
+}
